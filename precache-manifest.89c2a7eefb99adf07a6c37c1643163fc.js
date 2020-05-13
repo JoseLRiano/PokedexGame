2dfd70bc2bde6dc3c246b6a44ea76cb0295e789f
JoseLRiano <joselriano@yahoo.com>
2020-05-13 04:40:03 -0400

    Updates

---
+++ precache-manifest.89c2a7eefb99adf07a6c37c1643163fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8e84a777a61c4d9d5921980a1ce6b9",
+    "url": "/PokedexGame/index.html"
+  },
+  {
+    "revision": "979b85c38b02c9e664ae",
+    "url": "/PokedexGame/static/css/main.450ec61b.chunk.css"
+  },
+  {
+    "revision": "29dd449d36552d9e4e4c",
+    "url": "/PokedexGame/static/js/2.bb82f63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokedexGame/static/js/2.bb82f63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979b85c38b02c9e664ae",
+    "url": "/PokedexGame/static/js/main.ac363703.chunk.js"
+  },
+  {
+    "revision": "3c7dae098cd53647ed75",
+    "url": "/PokedexGame/static/js/runtime-main.89cad161.js"
+  }
+]);
